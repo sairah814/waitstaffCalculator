bcf102994446c715baa01f8b49112aec4f9f2177
sairah814 <sairah814@gmail.com>
2016-05-08 20:48:05 -0400

    version1 working

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 angular.module('myApp', [])
 
+//service keeping account of meal numbers and current average of meals
+//controller adds transactions and updates account in service
 .controller('ctrl', ['$scope', function ($scope) {
-    var mealcount = 0;
+    var totalmeals = 0;
     var tiptotal = 0;
     var average = 0;
     $scope.cancelForm = function () {
@@ -9,16 +11,21 @@ angular.module('myApp', [])
         $scope.taxrate = '';
         $scope.tippercent = '';
     };
-    $scope.submit = function () {
-        mealcount++;
-        $scope.subtotal = $scope.baseamount + ($scope.taxrate / 100) * $scope.baseamount;
-        $scope.tip = $scope.subtotal * ($scope.tippercent / 100);
-        console.log($scope.subtotal);
+
+    $scope.addTransaction = function () {
+
+        $scope.currentSubtotal = $scope.baseamount + ($scope.taxrate / 100) * $scope.baseamount;
+        $scope.currentTip = $scope.currentSubtotal * ($scope.tippercent / 100);
+        $scope.currentTotal = $scope.currentSubtotal + $scope.currentTip;
+        totalmeals++;
+        tiptotal = tiptotal + $scope.currentTip;
+        average = tiptotal / totalmeals;
+
+        $scope.cumulativeTips = tiptotal;
+        $scope.cumulativeAverage = average;
+        $scope.cumulativeMeals = totalmeals;
     };
 
-    $scope.tipcalculator = function () {
-
-    };
     }])
 
 ;
