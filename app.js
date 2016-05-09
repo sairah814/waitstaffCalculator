6ff39948a457272acd9b5ea356b2d8ff9f1bc50d
sairah814 <sairah814@gmail.com>
2016-05-08 20:29:44 -0400

    customer-charges v1

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 angular.module('myApp', [])
-    .controller($scope) {
+
+.controller('ctrl', ['$scope', function ($scope) {
+    var mealcount = 0;
+    var tiptotal = 0;
+    var average = 0;
+    $scope.cancelForm = function () {
+        $scope.baseamount = '';
+        $scope.taxrate = '';
+        $scope.tippercent = '';
+    };
+    $scope.submit = function () {
+        mealcount++;
+        $scope.subtotal = $scope.baseamount + ($scope.taxrate / 100) * $scope.baseamount;
+        $scope.tip = $scope.subtotal * ($scope.tippercent / 100);
+        console.log($scope.subtotal);
+    };
+
+    $scope.tipcalculator = function () {
 
     };
+    }])
+
+;
