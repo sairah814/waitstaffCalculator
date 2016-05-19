f5e90dcc97551daf40ae669de62f1a5666c7b4b1
sairah814 <sairah814@gmail.com>
2016-05-18 23:50:42 -0400

    got basic routing in

--- app.js
+++ app.js
@@ -1,49 +1,77 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+                templateUrl: 'home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/newmeal', {
+                templateUrl: 'newmeal.html',
+                controller: 'NewMealCtrl',
+            })
+            .when('/myearnings', {
+                templateUrl: 'myearnings.html',
+                controller: 'MyEarningsCtrl'
+            })
+            .when('/error', {
+                templateUrl: 'error.html',
+                controller: 'HomeCtrl'
+            })
+            .otherwise({
+                redirectTo: '/error'
+            });
+    }])
+    //service keeping account of meal numbers and current average of meals
+    //controller adds transactions and updates account in service
+    .controller('HomeCtrl', ['$scope', function ($scope) {
 
-//service keeping account of meal numbers and current average of meals
-//controller adds transactions and updates account in service
-.controller('ctrl', ['$scope', function ($scope) {
-    var data = {};
-    var totalmeals = 0;
-    var tiptotal = 0;
-    var average = 0;
-    $scope.cancelForm = function () {
-        $scope.baseamount = '';
-        $scope.taxrate = '';
-        $scope.tippercent = '';
-    };
+    }])
+    .controller('NewMealCtrl', ['$scope', function ($scope) {
 
-    $scope.clearEarnings = function () {
-        tiptotal = 0;
-        average = 0;
-        totalmeals = 0;
-        $scope.currentSubtotal = 0;
-        $scope.currentTip = 0;
-        $scope.currentTotal = 0;
-        $scope.cumulativeAverage = 0;
-        $scope.cumulativeMeals = 0;
-        $scope.cumulativeTips = 0;
-    }
+    }])
+    .controller('MyEarningsCtrl', ['$scope', function ($scope) {
 
-    $scope.addTransaction = function () {
+    }])
+    .controller('ctrl', ['$scope', function ($scope) {
+        var totalmeals = 0;
+        var tiptotal = 0;
+        var average = 0;
+        $scope.cancelForm = function () {
+            $scope.baseamount = '';
+            $scope.taxrate = '';
+            $scope.tippercent = '';
+        };
 
-        var base = parseFloat($scope.baseamount);
-        var tip = parseFloat($scope.tippercent);
-        var tax = parseFloat($scope.taxrate);
+        $scope.clearEarnings = function () {
+            tiptotal = 0;
+            average = 0;
+            totalmeals = 0;
+            $scope.currentSubtotal = 0;
+            $scope.currentTip = 0;
+            $scope.currentTotal = 0;
+            $scope.cumulativeAverage = 0;
+            $scope.cumulativeMeals = 0;
+            $scope.cumulativeTips = 0;
+        }
 
-        $scope.currentSubtotal = base + (tax / 100) * base;
-        $scope.currentTip = $scope.currentSubtotal * (tip / 100);
-        $scope.currentTotal = $scope.currentSubtotal + $scope.currentTip;
-        totalmeals++;
-        tiptotal = tiptotal + $scope.currentTip;
-        average = tiptotal / totalmeals;
+        $scope.addTransaction = function () {
 
-        $scope.cumulativeTips = tiptotal;
-        $scope.cumulativeAverage = average;
-        $scope.cumulativeMeals = totalmeals;
-    };
-    $scope.reset = function () {
-        this.cancelForm();
-        this.clearEarnings();
-    };
+            var base = parseFloat($scope.baseamount);
+            var tip = parseFloat($scope.tippercent);
+            var tax = parseFloat($scope.taxrate);
+
+            $scope.currentSubtotal = base + (tax / 100) * base;
+            $scope.currentTip = $scope.currentSubtotal * (tip / 100);
+            $scope.currentTotal = $scope.currentSubtotal + $scope.currentTip;
+            totalmeals++;
+            tiptotal = tiptotal + $scope.currentTip;
+            average = tiptotal / totalmeals;
+
+            $scope.cumulativeTips = tiptotal;
+            $scope.cumulativeAverage = average;
+            $scope.cumulativeMeals = totalmeals;
+        };
+        $scope.reset = function () {
+            this.cancelForm();
+            this.clearEarnings();
+        };
     }]);
